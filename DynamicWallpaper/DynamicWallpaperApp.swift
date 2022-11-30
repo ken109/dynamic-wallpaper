10ba0335adea55c8f1ff6c636abd4439ba45e01f
Kensuke Kubo <kensukekubo19@gmail.com>
2022-11-30 12:58:05 +0900

    Initial Commit

---
+++ DynamicWallpaper/DynamicWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicWallpaperApp.swift
+//  DynamicWallpaper
+//
+//  Created by Kensuke Kubo on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct DynamicWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
